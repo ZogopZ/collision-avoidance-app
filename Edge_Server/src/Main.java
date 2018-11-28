6df3db0edd06000e8ee244cd3af3e767b6ac14f1
Zois Zogopoulos <std06257@di.uoa.gr>
2018-11-28 14:03:09 +0200

    Edge server connectivity
     -added connectivity to a localhost server (to be backhaul server)
     -added download file option from server

--- Edge_Server/src/Main.java
+++ Edge_Server/src/Main.java
@@ -1,10 +1,31 @@
+import java.io.*;
+import java.net.Socket;
 
 public class Main
 {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World.");
+        try
+        {
+            Socket bhaul_socket;
+            InputStream inSocket;
+            OutputStream outFile;
+            String host = "127.0.0.1";                     //This is the localhost. Must be changed to Backhaul server's ip.
+            bhaul_socket = new Socket(host, 4444);    //Connect to Backhaul server's port 4444.
+            inSocket = bhaul_socket.getInputStream();      //Store socket's data in inSocket.
+            File training_set = new File("training_set.csv");
+            outFile = new FileOutputStream(training_set);  //Use outFile stream to write data to created file.
+
+            byte[] bytes = new byte[16*1024];
+            int count;
+            while ((count = inSocket.read(bytes)) > 0)     //Read data from Backhaul server and write it to file.
+            {
+                outFile.write(bytes, 0, count);
+            }
+            bhaul_socket.close();                          //Close connection with Backhaul server.
+        }
+        catch (Exception ex) {ex.printStackTrace();}
     }
 }
 
