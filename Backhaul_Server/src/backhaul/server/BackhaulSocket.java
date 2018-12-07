eaa961c15970729aee26a68c4881b6abc0c6877a
Zois Zogopoulos <std06257@di.uoa.gr>
2018-12-07 20:36:10 +0200

    Updated files for websocker server shutdown
    -modified file BackhaulSocket.java in /ditapp/Backhaul_Server/src/backhaul/server/
    -modified file BackhaulSocketServer.java in /ditapp/Backhaul_Server/src/backhaul/server/
    -added Thread.sleep(s) for better output

--- Backhaul_Server/src/backhaul/server/BackhaulSocket.java
+++ Backhaul_Server/src/backhaul/server/BackhaulSocket.java
@@ -4,10 +4,15 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.websocket.server.WebSocketHandler;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
-public class BackhaulSocket {
+
+public class BackhaulSocket
+{
+    public static int serverStop;
 
     public static void connect() throws Exception
     {
+        System.out.println("-websocket server initialization");
+
         Server server = new Server(8080);
         WebSocketHandler wsHandler = new WebSocketHandler()
         {
@@ -19,6 +24,14 @@ public class BackhaulSocket {
         };
         server.setHandler(wsHandler);
         server.start();
-        server.join();
+        System.out.println("-waiting for edge server...");
+        while (serverStop == 0)
+        {
+            Thread.sleep(1000);
+        }
+        Thread.sleep(4000);
+        System.out.println("-websocket server will now shut down");
+        server.stop();
     }
+
 }
--- Backhaul_Server/src/backhaul/server/BackhaulSocketServer.java
+++ Backhaul_Server/src/backhaul/server/BackhaulSocketServer.java
@@ -18,7 +18,7 @@ public class BackhaulSocketServer
     @OnWebSocketClose
     public void onClose(int statusCode, String reason)
     {
-        System.out.println("Close: statusCode=" + statusCode + ", reason=" + reason);
+        System.out.println("-edge server just exited");
     }
 
     @OnWebSocketError
@@ -28,18 +28,23 @@ public class BackhaulSocketServer
     }
 
     @OnWebSocketConnect
-    public void onConnect(Session session) throws Exception
-    {
-        System.out.println("Connect: " + session.getRemoteAddress().getAddress());
-        File file = new File("training_set.csv");
-        String contents = new String(Files.readAllBytes(Paths.get(file.getName())));
-        session.getRemote().sendString(contents);
+    public void onConnect(Session session) throws InterruptedException {
+        System.out.println("-edge server is now connected");
+        Thread.sleep(500);
     }
 
 
     @OnWebSocketMessage
-    public void onMessage(String message)
-    {
-        System.out.println("Message: " + message);
+    public void onMessage(Session session, String message) throws IOException, InterruptedException {
+        System.out.println("-incoming message ---> " + message);
+        Thread.sleep(1000);
+        System.out.println("-uploading training set to edge server");
+        File file = new File("training_set.csv");
+        String contents = new String(Files.readAllBytes(Paths.get(file.getName())));
+        session.getRemote().sendString(contents);
+        Thread.sleep(2500);
+        System.out.println("-file was uploaded successfully");
+        Thread.sleep(1000);
+        BackhaulSocket.serverStop = 1;
     }
 }
